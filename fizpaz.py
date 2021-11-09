97ebcab504355741bc64a9a08e2d43d05fa2944a
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2021-11-09 13:00:00 +0200

    Add files via upload

---
+++ fizpaz.py
@@ -0,0 +1,9 @@
+for n in range(1,101):
+    if(n%5==0 and n%3==0):
+        print("FIZE BAZE")
+    elif(n%5==0):
+        print("BAZE")
+    elif(n%3==0):
+        print("FIZE")
+    else :
+        print(n)
